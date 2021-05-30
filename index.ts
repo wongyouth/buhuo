e79941adbae9d4aa182c726641446c9695e7f9b2
Ryan Wang <wongyouth@gmail.com>
2021-05-30 11:34:28 +0800

    misc: add build task and upgrade pino

--- index.ts
+++ index.ts
@@ -1,9 +1,3 @@
 import { server } from './src/server'
 
-server.listen(4000, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
-})
+server.listen(4000)
